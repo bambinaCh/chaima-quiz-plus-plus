47a021f8a5f9804548287164db5fe10f48fc300b
bambinaCh <chaimaa.jarite@gmail.com>
2022-04-14 09:44:04 +0200

    kei ahnig

--- index.php
+++ index.php
@@ -4,22 +4,27 @@
     <?php include 'inc/header.php' ?>
 
     <main>
-        <?php
-        echo "<div class='px-4 py-5 my-5 text-center'>";
-        echo "<svg class='d-block mx-auto mb-4' stroke='currentColor' fill='currentColor' stroke-width='0' viewBox='0 0 24 24' alt='' width='72' height='57' xmlns='http://www.w3.org/2000/svg'> ";
-        echo "<circle cx='4.5' cy='9.5' r='2.5'></circle><circle cx='9' cy='5.5' r='2.5'></circle><circle cx='15' cy='5' r='2.5'></circle><circle cx='19.5' cy='9.5' r='2.5'></circle><path d='M17.34 14.86c-.87-1.02-1.6-1.89-2.48-2.91-.46-.54-1.05-1.08-1.75-1.32-.11-.04-.22-.07-.33-.09-.25-.04-.52-.04-.78-.04s-.53 0-.79.05c-.11.02-.22.05-.33.09-.7.24-1.28.78-1.75 1.32-.87 1.02-1.6 1.89-2.48 2.91-1.31 1.31-2.92 2.76-2.62 4.79.29 1.02 1.02 2.03 2.33 2.32.73.15 3.06-.44 5.54-.44h.18c2.48 0 4.81.58 5.54.44 1.31-.29 2.04-1.31 2.33-2.32.31-2.04-1.3-3.49-2.61-4.8z'></path></svg> ";
+        
+        <div class='px-4 py-5 my-5 text-center'>
+            <svg class='d-block mx-auto mb-4' stroke='currentColor' fill='currentColor' stroke-width='0' viewBox='0 0 24 24' alt='' width='72' height='57' xmlns='http://www.w3.org/2000/svg'>
+                <circle cx='4.5' cy='9.5' r='2.5'></circle>
+                <circle cx='9' cy='5.5' r='2.5'></circle>
+                <circle cx='15' cy='5' r='2.5'></circle>
+                <circle cx='19.5' cy='9.5' r='2.5'></circle>
+                <path d='M17.34 14.86c-.87-1.02-1.6-1.89-2.48-2.91-.46-.54-1.05-1.08-1.75-1.32-.11-.04-.22-.07-.33-.09-.25-.04-.52-.04-.78-.04s-.53 0-.79.05c-.11.02-.22.05-.33.09-.7.24-1.28.78-1.75 1.32-.87 1.02-1.6 1.89-2.48 2.91-1.31 1.31-2.92 2.76-2.62 4.79.29 1.02 1.02 2.03 2.33 2.32.73.15 3.06-.44 5.54-.44h.18c2.48 0 4.81.58 5.54.44 1.31-.29 2.04-1.31 2.33-2.32.31-2.04-1.3-3.49-2.61-4.8z'></path>
+            </svg>
 
-        echo " <h1 class='display-5 fw-bold'>Tierliebhaber?</h1> ";
+            <h1 class='display-5 fw-bold'>Tierliebhaber?</h1>
 
-        echo " <form action='/page1.php' method='post'> ";
-        echo " <div class='col-lg-6 mx-auto'> ";
-        echo "<p class='lead mb-4'>Nimm teil an der Quiz und prüfe dein Wissen! ";
-        echo "<div class='d-grid gap-2 d-sm-flex justify-content-sm-center'> ";
-        echo " <button type='submit' class='btn btn-outline-secondary btn-lg px-4'>QUIZ STARTEN</button> ";
-        echo "</div>";
-        echo "</div>";
-        echo " </form> ";
-        ?>
+            <form action='/page1.php' method='post'>
+                <div class='col-lg-6 mx-auto'>
+                    <p class='lead mb-4'>Nimm teil an der Quiz und prüfe dein Wissen!</p>
+                    <div class='d-grid gap-2 d-sm-flex justify-content-sm-center'>
+                        <button type='submit' class='btn btn-outline-secondary btn-lg px-4'>QUIZ STARTEN</button>
+                    </div>
+                </div>
+            </form>
+
     </main>
 
 </body>
--- page1.php
+++ page1.php
@@ -9,32 +9,32 @@ $_SESSION["score"] = 0;
     <?php include 'inc/header.php' ?>
 
     <main>
-        <?php
-        echo "<div class='px-4 py-5 my-5 text-center'>";
-        echo " <img class='question-image' src='../img/rings.jpg' alt=''> ";
 
-        echo " <h1 class='display-5 fw-bold'>Frage 1 </h1> ";
+        <div class='px-4 py-5 my-5 text-center'>
+            <img class='question-image' src='../img/rings.jpg' alt=''>
 
-        echo " <div class='col-lg-6 mx-auto'> ";
-        echo "<p class='lead mb-4'> How many rings has the olympics symbol?</p> ";
+            <h1 class='display-5 fw-bold'>Frage <?php echo $currentQuestion ?> 1 </h1>
 
-        echo " <form action='/page2.php' method='post' onsubmit='return validateNumber1();'> ";
+            <div class='col-lg-6 mx-auto'>
+                <p class='lead mb-4'> How many rings has the olympics symbol?</p>
 
-        echo " <input type='number' name='number-text' id='number-text' class='number-imput'><br> ";
-        echo " <input type='hidden' name='lastPageID' value='question-1'> ";
-        echo " <input type='hidden' name='points' id='rings' value=''> ";
+                <form action='/page2.php' method='post' onsubmit='return validateNumber1();'>
 
-        echo " <p id='validation-warning' class='warning'></p> ";
+                    <input type='number' name='number-text' id='number-text' class='number-imput'><br>
+                    <input type='hidden' name='lastPageID' value='question-1'>
+                    <input type='hidden' name='points' id='rings' value=''>
 
-        echo "<div class='d-grid gap-2 d-sm-flex justify-content-sm-center'> ";
-        echo " <button type='submit' class='btn btn-outline-secondary btn-lg px-4'>WEITER</button> ";
-        echo "</div>";
-        echo "</div>";
-        echo "</div>";
-        echo " </form> ";
+                    <p id='validation-warning' class='warning'></p>
+
+                    <div class='d-grid gap-2 d-sm-flex justify-content-sm-center'>
+                        <button type='submit' class='btn btn-outline-secondary btn-lg px-4'>WEITER</button>
+                    </div>
+            </div>
+        </div>
+        </form>
 
 
-        echo " <script src='js/main.js'></script> ";
+        <script src='js/main.js'></script>
         ?>
     </main>
 
